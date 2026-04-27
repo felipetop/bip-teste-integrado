82de37ca2c62d981ff702705491670a00926f7ea
felipetop <felipejorgetop@gmail.com>
2026-04-27 18:33:16 -0300

    chore: estrutura inicial do projeto multi-módulo

---
+++ backend-module/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO BENEFICIO (NOME, DESCRICAO, VALOR, ATIVO, VERSION) VALUES
+('Vale Refeição', 'Crédito mensal para alimentação', 1000.00, TRUE, 0),
+('Vale Transporte', 'Auxílio para deslocamento', 500.00, TRUE, 0);
